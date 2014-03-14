7dc2ccd99c831200d0e44cebf4940da59e72080e
Gilles Duboscq <duboscq@ssw.jku.at>
2014-03-13 17:56:29 -0700

    make the DuplicateSuppressingStream periodically print while it is filtering output

--- mx.py
+++ mx.py
@@ -1823,6 +1823,8 @@ class DuplicateSuppressingStream:
         self.restrictTo = restrictTo
         self.seen = set()
         self.out = out
+        self.currentFilteredLineCount = 0
+        self.currentFilteredTime = None
 
     def isSuppressionCandidate(self, line):
         if self.restrictTo:
@@ -1836,9 +1838,18 @@ class DuplicateSuppressingStream:
     def write(self, line):
         if self.isSuppressionCandidate(line):
             if line in self.seen:
+                self.currentFilteredLineCount += 1
+                if self.currentFilteredTime:
+                    if time.time() - self.currentFilteredTime > 1 * 60:
+                        self.out.write("  Filtered " + str(self.currentFilteredLineCount) + " repeated lines...\n")
+                        self.currentFilteredTime = time.time()
+                else:
+                    self.currentFilteredTime = time.time()
                 return
             self.seen.add(line)
+        self.currentFilteredLineCount = 0
         self.out.write(line)
+        self.currentFilteredTime = None
 
 """
 A JavaCompliance simplifies comparing Java compliance values extracted from a JDK version string.
