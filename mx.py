f4f0a9098baf2bc9c793ebe62a59f32fe7e0d466
Bernhard Urban <bernhard.urban@jku.at>
2014-06-12 11:40:33 +0200

    clean: delete distributions

--- mx.py
+++ mx.py
@@ -3343,6 +3343,7 @@ def clean(args, parser=None):
     parser.add_argument('--no-native', action='store_false', dest='native', help='do not clean native projects')
     parser.add_argument('--no-java', action='store_false', dest='java', help='do not clean Java projects')
     parser.add_argument('--projects', action='store', help='comma separated projects to clean (omit to clean all projects)')
+    parser.add_argument('--no-dist', action='store_false', dest='dist', help='do not delete distributions')
 
     args = parser.parse_args(args)
 
@@ -3356,6 +3357,10 @@ def clean(args, parser=None):
         if get_os() == 'windows':
             path = unicode("\\\\?\\" + dirPath)
         shutil.rmtree(path)
+
+    def _rmIfExists(name):
+        if os.path.isfile(name):
+            os.unlink(name)
 
     for p in projects:
         if p.native:
@@ -3379,6 +3384,13 @@ def clean(args, parser=None):
                 config = TimeStampFile(join(p.suite.mxDir, configName))
                 if config.exists():
                     os.unlink(config.path)
+
+    if args.dist:
+        for d in _dists.keys():
+            log('Removing distribution {0}...'.format(d))
+            _rmIfExists(distribution(d).path)
+            _rmIfExists(distribution(d).sourcesPath)
+
 
     if suppliedParser:
         return args
