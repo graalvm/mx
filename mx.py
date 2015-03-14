19809ef4515c1a18010de3dbaf50af51145dad53
Mick Jordan <mick.jordan@oracle.com>
2015-03-13 20:18:31 -0700

    version 2.9.3

--- mx.py
+++ mx.py
@@ -6593,7 +6593,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1)
 
-version = VersionSpec("2.9.2")
+version = VersionSpec("2.9.3")
 
 currentUmask = None
 
