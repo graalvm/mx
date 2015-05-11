b7023220eb1c8598712e76a8edb883000aa843d3
Christian Humer <christian.humer@oracle.com>
2015-05-11 12:00:21 +0200

    Bump version to 3.1.1.

--- mx.py
+++ mx.py
@@ -6857,7 +6857,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1)
 
-version = VersionSpec("3.1.0")
+version = VersionSpec("3.1.1")
 
 currentUmask = None
 
