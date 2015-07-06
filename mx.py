077a3c74ec514adb0e613bc825e33f520e2919d5
Doug Simon <doug.simon@oracle.com>
2015-07-06 12:52:12 +0200

    read class files as binaries when creating archives

--- mx.py
+++ mx.py
@@ -441,7 +441,7 @@ class Distribution(Dependency):
                             else:
                                 for f in files:
                                     arcname = join(relpath, f).replace(os.sep, '/')
-                                    with open(join(root, f), 'r') as fp:
+                                    with open(join(root, f), 'rb') as fp:
                                         contents = fp.read()
                                     if not self.archiveparticipant or not self.archiveparticipant.__add__(arcname, contents):
                                         arc.zf.writestr(arcname, contents)
