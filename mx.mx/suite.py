66836134a08c1fc733cccef7e24718d1919875d3
Doug Simon <doug.simon@oracle.com>
2015-09-04 16:27:24 +0200

    use public https urls

--- mx.mx/suite.py
+++ mx.mx/suite.py
@@ -7,20 +7,20 @@ suite = {
 
     "JACOCOAGENT" : {
       "path" : "lib/jacocoagent.jar",
-      "urls" : ["http://lafo.ssw.uni-linz.ac.at/jacoco/jacocoagent-0.7.1-1.jar"],
+      "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/jacoco/jacocoagent-0.7.1-1.jar"],
       "sha1" : "2f73a645b02e39290e577ce555f00b02004650b0",
     },
 
     "JACOCOREPORT" : {
       "path" : "lib/jacocoreport.jar",
-      "urls" : ["http://lafo.ssw.uni-linz.ac.at/jacoco/jacocoreport-0.7.1-2.jar"],
+      "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/jacoco/jacocoreport-0.7.1-2.jar"],
       "sha1" : "a630436391832d697a12c8f7daef8655d7a1efd2",
     },
 
     "FINDBUGS_DIST" : {
       "path" : "lib/findbugs-dist-3.0.0.zip",
       "urls" : [
-        "http://lafo.ssw.uni-linz.ac.at/graal-external-deps/findbugs-3.0.0.zip",
+        "https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/findbugs-3.0.0.zip",
         "http://sourceforge.net/projects/findbugs/files/findbugs/3.0.0/findbugs-3.0.0.zip/download",
       ],
       "sha1" : "6e56d67f238dbcd60acb88a81655749aa6419c5b",
@@ -29,14 +29,14 @@ suite = {
     "JUNIT" : {
       "path" : "lib/junit-4.11.jar",
       "urls" : [
-        "http://lafo.ssw.uni-linz.ac.at/graal-external-deps/junit-4.11.jar",
+        "https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/junit-4.11.jar",
         "https://search.maven.org/remotecontent?filepath=junit/junit/4.11/junit-4.11.jar",
       ],
       "sha1" : "4e031bb61df09069aeb2bffb4019e7a5034a4ee0",
       "eclipse.container" : "org.eclipse.jdt.junit.JUNIT_CONTAINER/4",
       "sourcePath" : "lib/junit-4.11-sources.jar",
       "sourceUrls" : [
-        "http://lafo.ssw.uni-linz.ac.at/graal-external-deps/junit-4.11-sources.jar",
+        "https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/junit-4.11-sources.jar",
         "https://search.maven.org/remotecontent?filepath=junit/junit/4.11/junit-4.11-sources.jar",
       ],
       "sourceSha1" : "28e0ad201304e4a4abf999ca0570b7cffc352c3c",
@@ -52,7 +52,7 @@ suite = {
     "CHECKSTYLE" : {
       "path" : "lib/checkstyle-6.0-all.jar",
       "urls" : [
-        "http://lafo.ssw.uni-linz.ac.at/graal-external-deps/checkstyle-6.0-all.jar",
+        "https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/checkstyle-6.0-all.jar",
         "jar:http://sourceforge.net/projects/checkstyle/files/checkstyle/6.0/checkstyle-6.0-bin.zip/download!/checkstyle-6.0/checkstyle-6.0-all.jar",
       ],
       "sha1" : "2bedc7feded58b5fd65595323bfaf7b9bb6a3c7a",
@@ -67,13 +67,13 @@ suite = {
     "HAMCREST" : {
       "path" : "lib/hamcrest-core-1.3.jar",
       "urls" : [
-        "http://lafo.ssw.uni-linz.ac.at/graal-external-deps/hamcrest-core-1.3.jar",
+        "https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/hamcrest-core-1.3.jar",
         "https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar",
       ],
       "sha1" : "42a25dc3219429f0e5d060061f71acb49bf010a0",
       "sourcePath" : "lib/hamcrest-core-1.3-sources.jar",
       "sourceUrls" : [
-        "http://lafo.ssw.uni-linz.ac.at/graal-external-deps/hamcrest-core-1.3-sources.jar",
+        "https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/hamcrest-core-1.3-sources.jar",
         "https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3-sources.jar",
       ],
       "sourceSha1" : "1dc37250fbc78e23a65a67fbbaf71d2e9cbc3c0b",
