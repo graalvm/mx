00806f948397179de924690d45c10929fc8d0b72
Doug Simon <doug.simon@oracle.com>
2015-11-13 23:19:44 +0100

    bumped version to 5.6.4 for previous changeset

--- mx.py
+++ mx.py
@@ -11468,7 +11468,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1)
 
-version = VersionSpec("5.6.3")
+version = VersionSpec("5.6.4")
 
 currentUmask = None
 
