3aa856ea450015b96f77da0b9c96fb7ab38e5b08
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2015-11-20 16:08:17 +0100

    mx sigtest --check all to check API changes against previously generated (via mx sigtest --generate) snapshot files

--- mx_sigtest.py
+++ mx_sigtest.py
@@ -39,16 +39,20 @@ def _should_test_project(p):
 def sigtest(args, suite=None, projects=None):
     parser = ArgumentParser(prog='mx sigtest')
     parser.add_argument('--generate', action='store_true', help='Generates signature files for projects with API')
+    parser.add_argument('--check', action='store', help='Check <binary|all> against existing signature files', default='binary')
 
     args = parser.parse_args(args)
 
     if args.generate:
         _sigtest_generate(args)
     else:
-        parser.print_help()
+        if args.check:
+            _sigtest_check(args.check, args)
+        else:
+            parser.print_help()
 
 def _sigtest_generate(args, suite=None, projects=None):
-    """run sigtest against Java projects with API"""
+    """run sigtest generator for Java projects with API"""
     sigtestlib = mx.library('SIGTEST').get_path(resolve=True)
     nonTestProjects = [p for p in mx.projects() if _should_test_project(p)]
     if not nonTestProjects:
@@ -69,4 +73,32 @@ def _sigtest_generate(args, suite=None, projects=None):
         if not exists(sigtestResults):
             mx.abort('Cannot generate ' + sigtestResults)
         mx.log("Sigtest snapshot generated to " + sigtestResults)
+    return 0
+
+def _sigtest_check(checktype, args, suite=None, projects=None):
+    """run sigtest against Java projects with API"""
+    sigtestlib = mx.library('SIGTEST').get_path(resolve=True)
+    nonTestProjects = [p for p in mx.projects() if _should_test_project(p)]
+    if not nonTestProjects:
+        return 1
+    javaCompliance = max([p.javaCompliance for p in nonTestProjects])
+
+    failed = None
+    for p in nonTestProjects:
+        sigtestResults = p.dir + os.sep + 'sigtest-report.out'
+        if not os.path.exists(sigtestResults):
+            continue
+        cmd = ['-cp', mx._cygpathU2W(sigtestlib), 'com.sun.tdk.signaturetest.SignatureTest',
+            '-Static', '-Mode', 'bin', '-FileName', sigtestResults,
+            '-ClassPath', mx.classpath(p) + os.pathsep + mx.get_jdk(javaCompliance).bootclasspath(),
+        ]
+        if checktype != 'all':
+            cmd.append('-b')
+        for pkg in mx.find_packages(p):
+            cmd = cmd + ['-PackageWithoutSubpackages', pkg]
+        exitcode = mx.run_java(cmd, nonZeroIsFatal=False, jdk=mx.get_jdk(javaCompliance))
+        if exitcode != 95:
+            failed = sigtestResults
+    if failed:
+        mx.abort('Signature error in ' + failed)
     return 0
