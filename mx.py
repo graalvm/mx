85a80c76ba90aceca9586e80535854a0f566b357
Christian Humer <christian.humer@oracle.com>
2016-06-01 13:10:38 +0200

    Set version to 5.29.0 (reverted from commit 6acce2bc2fcdcfa53ffde44f4cfdd6a2005eb741)

--- mx.py
+++ mx.py
@@ -13761,7 +13761,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1)
 
-version = VersionSpec("5.29.0")
+version = VersionSpec("5.29.4")
 
 currentUmask = None
 
