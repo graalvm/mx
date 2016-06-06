df8125971222a010eee70bc8671c5d720e561059
Paul Wögerer <paul.woegerer@oracle.com>
2016-06-06 17:37:45 +0200

    Fix building with `gate --tags build,...`
    Running gate checks with `gate --tags build,...` causes problems if the
    suite contains native projects. Also the current handling is asymmetric
    as the cleaning step does cleaning also for native projects. One would
    expect that the subsequent building then also takes care of building
    native projects.

--- mx_gate.py
+++ mx_gate.py
@@ -278,7 +278,7 @@ def gate(args):
                         _warn_or_abort('JDT environment variable not set. Cannot execute BuildJavaWithEcj task.', args.strict_mode)
 
         with Task('BuildJavaWithJavac', tasks, tags=[Tags.build, Tags.fullbuild]) as t:
-            if t: mx.command_function('build')(['-p', '--warning-as-error', '--no-native', '--force-javac'])
+            if t: mx.command_function('build')(['-p', '--warning-as-error', '--force-javac'])
 
         with Task('IDEConfigCheck', tasks, tags=[Tags.fullbuild]) as t:
             if t:
