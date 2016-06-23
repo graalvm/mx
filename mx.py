9df4d2cc230333a28960d592c878349bdbac5301
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2016-06-23 17:16:08 +0200

    Bump version

--- mx.py
+++ mx.py
@@ -13809,7 +13809,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1)
 
-version = VersionSpec("5.31.0")
+version = VersionSpec("5.31.1")
 
 currentUmask = None
 
