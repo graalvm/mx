0bfa975a17a58efdf8dd60d5419549519e0a0371
Doug Simon <doug.simon@oracle.com>
2016-08-08 22:11:12 +0200

    guard against self.path is None in Archiver

--- mx.py
+++ mx.py
@@ -9618,21 +9618,23 @@ class Archiver(SafeFileCreation):
         self.kind = kind
 
     def __enter__(self):
-        SafeFileCreation.__enter__(self)
-        if self.tmpPath:
-            if self.kind == 'zip':
-                self.zf = zipfile.ZipFile(self.tmpPath, 'w')
-            elif self.kind == 'tar':
-                self.zf = tarfile.open(self.tmpPath, 'w')
-            elif self.kind == 'tgz':
-                self.zf = tarfile.open(self.tmpPath, 'w:gz')
-            else:
-                abort('unsupported archive kind: ' + self.kind)
+        if self.path:
+            SafeFileCreation.__enter__(self)
+            if self.tmpPath:
+                if self.kind == 'zip':
+                    self.zf = zipfile.ZipFile(self.tmpPath, 'w')
+                elif self.kind == 'tar':
+                    self.zf = tarfile.open(self.tmpPath, 'w')
+                elif self.kind == 'tgz':
+                    self.zf = tarfile.open(self.tmpPath, 'w:gz')
+                else:
+                    abort('unsupported archive kind: ' + self.kind)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        if self.zf:
-            self.zf.close()
+        if self.path:
+            if self.zf:
+                self.zf.close()
         SafeFileCreation.__exit__(self, exc_type, exc_value, traceback)
 
 def _archive(args):
