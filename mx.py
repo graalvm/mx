a6b0ab2068c8505882f9f3c69f0fad9fb306c106
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2016-09-06 16:49:35 +0200

    Bump version.

--- mx.py
+++ mx.py
@@ -14231,7 +14231,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1)
 
-version = VersionSpec("5.44.0")
+version = VersionSpec("5.44.1")
 
 currentUmask = None
 
