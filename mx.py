a7447c737aecd9e9d4fc4688cca14fc73b3e5784
Paul Wögerer <paul.woegerer@oracle.com>
2016-09-23 13:32:59 +0200

    Update version info to 5.46.5

--- mx.py
+++ mx.py
@@ -14279,7 +14279,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1)
 
-version = VersionSpec("5.46.4")
+version = VersionSpec("5.46.5")
 
 currentUmask = None
 
