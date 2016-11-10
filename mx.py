d6525ea1d18e65493c00fea8f00be586a58bb36a
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2016-11-10 22:08:58 +0100

    Bump minor version.

--- mx.py
+++ mx.py
@@ -14454,7 +14454,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1, killsig=signal.SIGINT)
 
-version = VersionSpec("5.54.0")
+version = VersionSpec("5.55.0")
 
 currentUmask = None
 
