1dc5c0cfc48b93df557fafd7c62fb21811f443e9
Doug Simon <doug.simon@oracle.com>
2016-12-13 14:35:07 +0100

    add exports for transitive annotation processors

--- mx.py
+++ mx.py
@@ -2490,9 +2490,19 @@ class JavacCompiler(JavacLikeCompiler):
             aps = project.annotation_processors()
             if aps:
                 exports = {}
+
                 for dep in classpath_entries(aps, preferProjects=True):
                     if dep.isJavaProject():
                         addExportArgs(dep, exports, '-J', jdk)
+
+                # An annotation processor may have a dependency on other annotation
+                # processors. The latter might need extra exports.
+                for dep in classpath_entries(aps, preferProjects=False):
+                    if dep.isJARDistribution() and dep.definedAnnotationProcessors:
+                        for apDep in dep.deps:
+                            if apDep.isJavaProject():
+                                addExportArgs(apDep, exports, '-J', jdk)
+
                 # If modules are exported for use by an annotation processor then
                 # they need to be boot modules since --add-exports can only be used
                 # for boot modules.
@@ -6755,7 +6765,7 @@ class SourceSuite(Suite):
             self.projects.append(p)
 
 
-        # Create a distribution for each project that defines annotation processors
+        # Record the projects that define annotation processors
         apProjects = {}
         for p in self.projects:
             if not p.isJavaProject():
