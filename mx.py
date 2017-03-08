8591f10088f9020aee4b854a766b05a8872924f7
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2017-03-08 14:52:47 +0100

    Bump mx version to 5.75.1.

--- mx.py
+++ mx.py
@@ -15093,7 +15093,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1, killsig=signal.SIGINT)
 
-version = VersionSpec("5.75.0")
+version = VersionSpec("5.75.1")
 
 currentUmask = None
 
