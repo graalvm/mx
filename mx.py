439441dcba10442e6a4dc81bf6e1ef0a3e9de9dc
Josef Eisl <josef.eisl@jku.at>
2017-04-06 13:22:33 +0200

    Bump mx version to 5.83.4

--- mx.py
+++ mx.py
@@ -15292,7 +15292,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1, killsig=signal.SIGINT)
 
-version = VersionSpec("5.83.3")
+version = VersionSpec("5.83.4")
 
 currentUmask = None
 
