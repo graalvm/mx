80b010e3e716360ce50df2bdcb13c508e064cc31
Christian Humer <christian.humer@oracle.com>
2017-05-02 13:39:44 +0200

    Bump mx version to 5.90.0

--- mx.py
+++ mx.py
@@ -15272,7 +15272,7 @@ def main():
         # no need to show the stack trace when the user presses CTRL-C
         abort(1, killsig=signal.SIGINT)
 
-version = VersionSpec("5.89.0")
+version = VersionSpec("5.90.0")
 
 currentUmask = None
 
