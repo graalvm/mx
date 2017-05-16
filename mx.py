1839ef0b0320b9abcb306f447db75365e52c923f
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2017-05-16 18:09:42 +0200

    Bump version

--- mx.py
+++ mx.py
@@ -15654,7 +15654,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.100.0") # Anniversary Edition
+version = VersionSpec("5.101.0")  # 12 herbs and spices!
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
