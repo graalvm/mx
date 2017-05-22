1bf8f8cd55483d6e0e2966c9ee4773a924c6465a
Cansu Kaynak <cansu.kaynak@oracle.com>
2017-05-22 11:09:46 +0200

    bumped version number

--- mx.py
+++ mx.py
@@ -15682,7 +15682,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.103.0")  # Try it!!!
+version = VersionSpec("5.104.0")  # Try it!!!
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
