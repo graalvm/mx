ef71e591e95c155f7b1ba330fa40bb1e3f5bb41d
Roland Schatz <roland.schatz@oracle.com>
2017-06-22 11:49:02 +0200

    Bump version.

--- mx.py
+++ mx.py
@@ -15798,7 +15798,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.114.5")  # Silver theory
+version = VersionSpec("5.114.6")  # Maven madness
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
