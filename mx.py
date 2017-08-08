3309f948b9b8e56f6be15956bb4c7736d57928dd
Josef Eisl <josef.eisl@jku.at>
2017-08-08 11:18:01 +0200

    Bump mx version

--- mx.py
+++ mx.py
@@ -15978,7 +15978,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.122.1")  # don't use WarnDeprecatedMixin in mx
+version = VersionSpec("5.122.2")  # backout/GR-4845
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
