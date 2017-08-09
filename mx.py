606c5323cf69176b8275d25f336f04be3db9d340
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2017-08-09 16:17:39 +0200

    All project to explicitly specify their JDK9 runtime - e.g. 'require static' dependencies

--- mx.py
+++ mx.py
@@ -16002,7 +16002,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.122.3")  # reenable JMHArchiveParticipant
+version = VersionSpec("5.123.0")  # support runtimeDeps
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
