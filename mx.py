038bd52656d2cd73ca3b7a522cb1e0baa8313fe4
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2017-08-17 15:22:10 +0200

    Increasing patch version of mx

--- mx.py
+++ mx.py
@@ -16005,7 +16005,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.124.3")  # full build deps
+version = VersionSpec("5.124.4")  # javadoc fixes
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
