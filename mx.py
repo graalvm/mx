380b11dd54734ec36a4be1f8a3dc28d422a7d095
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2017-08-25 16:58:41 +0200

    Bump version

--- mx.py
+++ mx.py
@@ -16037,7 +16037,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.124.8")  # Downloads
+version = VersionSpec("5.124.9")  # sforceimports
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
