992759f73c8dc735f7737740cc839c09b4028e21
Roland Schatz <roland.schatz@oracle.com>
2017-09-20 16:05:56 +0200

    Bump mx version.

--- mx.py
+++ mx.py
@@ -16188,7 +16188,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.127.1")  # unstrip
+version = VersionSpec("5.127.2")  # threadsafe print
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
