35eff0b764991a43f733c281665fa7f4e33fb43a
Benoit Daloze <benoit.d.daloze@oracle.com>
2017-10-03 19:40:16 +0200

    Remove ASM sources, they produce a lot of duplicated entries warnings
    * See https://gitlab.ow2.org/asm/asm/issues/317795 for the upstream bug.

--- mx.py
+++ mx.py
@@ -16272,7 +16272,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.128.1")  # handle more jdk.unsupported
+version = VersionSpec("5.128.2")  # no duplicated asm source warnings
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
