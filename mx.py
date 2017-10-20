5863ff6854c3fbbbd6f3e122facc7f79013b442c
Roland Schatz <roland.schatz@oracle.com>
2017-10-20 15:55:35 +0200

    Version bump.

--- mx.py
+++ mx.py
@@ -16313,7 +16313,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.128.5")  # native build dirs
+version = VersionSpec("5.128.6")  # vpath fix
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
