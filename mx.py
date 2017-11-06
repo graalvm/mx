398992bed6a41e29b2201a800e739f29bac22ef2
Paul Wögerer <paul.woegerer@oracle.com>
2017-11-06 13:52:58 +0100

    Bump version

--- mx.py
+++ mx.py
@@ -16404,7 +16404,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.128.9")  # maven deploy validation fix
+version = VersionSpec("5.129.0")  # branch ref based version resolving with 'git-bref:<branch-name>'
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
