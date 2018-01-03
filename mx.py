ca67870ee4ebf3a7a2110af69f7d5fde0452bcaa
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2018-01-03 18:14:27 +0100

    [GR-7607] Benchmarks: add some missing `super` calls

--- mx.py
+++ mx.py
@@ -16624,7 +16624,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.134.2")  # [GR-7589] specifying benchmark lists
+version = VersionSpec("5.134.3")  # super benchmarking
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
