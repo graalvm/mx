0f856a4e5574b9b55d6d02e87fbf11806b170fe1
Doug Simon <doug.simon@oracle.com>
2018-05-14 16:03:30 +0200

    bump minor version

--- mx.py
+++ mx.py
@@ -17932,7 +17932,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.162.1")  # GR-9857
+version = VersionSpec("5.163.0")  # GR-9819-revert
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
