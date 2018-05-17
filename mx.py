39538a2619cb8886b6a1c8929e47a3a7c3bbe2e2
Paul Wögerer <paul.woegerer@oracle.com>
2018-05-17 12:24:16 +0200

    bump mx version

--- mx.py
+++ mx.py
@@ -18000,7 +18000,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.165.0")  # GR-9819_attempt2
+version = VersionSpec("5.166.0")  # GR-9937
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
