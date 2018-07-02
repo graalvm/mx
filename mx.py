178ef4f7ab9d6965ac88f522097fc969a68b385e
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2018-07-02 16:09:27 +0200

    Make sure archivable results for libaries are not links
    Otherwise the link would be included in the archive which would fail.
    In particular, for libraries, there might be 2 link indirections.

--- mx.py
+++ mx.py
@@ -4295,7 +4295,7 @@ class ResourceLibrary(BaseLibrary):
         self.sha1 = sha1
 
     def getArchivableResults(self, use_relpath=True, single=False):
-        path = self.get_path(False)
+        path = realpath(self.get_path(False))
         yield path, _map_to_maven_dist_name(self.name) + '.' + get_file_extension(path)
 
     def getBuildTask(self, args):
@@ -4590,10 +4590,10 @@ class Library(BaseLibrary, ClasspathDependency):
         return LibraryDownloadTask(args, self)
 
     def getArchivableResults(self, use_relpath=True, single=False):
-        path = self.get_path(False)
+        path = realpath(self.get_path(False))
         yield path, _map_to_maven_dist_name(self.name) + '.' + get_file_extension(path)
         if not single:
-            src_path = self.get_source_path(False)
+            src_path = realpath(self.get_source_path(False))
             if src_path:
                 ext = get_file_extension(src_path)
                 if 'src' not in ext and 'source' not in ext:
