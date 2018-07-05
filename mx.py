af1032b2f7862359107623bc9e71169f2174562a
Boris Spasojevic <boris.spasojevic@oracle.com>
2018-07-05 11:09:56 +0200

    Version bump.

--- mx.py
+++ mx.py
@@ -17814,7 +17814,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.176.4")  # sigtest
+version = VersionSpec("5.177.0")  # GR-10728
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
