9e1c39ad164a4b74b44a005cc2631974587f0ed8
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2018-08-07 13:38:40 +0200

    Avoid data descriptors in JAR files
    JDK 8 and before has a bug in `ZipInputStream` that prevents it from
    reading JAR files with such descriptors in certain cases.

--- mx.py
+++ mx.py
@@ -1290,6 +1290,7 @@ class JARDistribution(Distribution, ClasspathDependency):
                         self.arcname = arcname
                         self.source = source
                         self._can_write = False
+
                     def __enter__(self):
                         arcname = self.arcname
                         source = self.source
@@ -1305,7 +1306,7 @@ class JARDistribution(Distribution, ClasspathDependency):
                             self.zf._provenance = {}
                         existingSource = self.zf._provenance.get(arcname, None)
                         if existingSource and existingSource != source:
-                            if arcname[-1] != os.path.sep:
+                            if arcname[-1] not in (os.path.sep, '/'):
                                 if self.source_zf and self.source_zf.read(arcname) == self.zf.read(arcname):
                                     logv(self.path + ': file ' + arcname + ' is already present\n  new: ' + source + '\n  old: ' + existingSource)
                                 else:
@@ -1332,10 +1333,13 @@ class JARDistribution(Distribution, ClasspathDependency):
                                     if guard:
                                         contents = source_zf.read(arcname)
                                         if not participants__add__(arcname, contents):
+                                            # The JDK's ZipInputStream will fail to read files with a data descriptor written by python's zipfile
+                                            info.flag_bits &= ~0x08
                                             arc.zf.writestr(info, contents)
 
                 def addFile(outputDir, relpath, archivePrefix):
                     arcname = join(archivePrefix, relpath).replace(os.sep, '/')
+                    assert arcname[-1] != '/'
                     if relpath.startswith(join('META-INF', 'services')):
                         service = basename(relpath)
                         assert dirname(relpath) == join('META-INF', 'services')
