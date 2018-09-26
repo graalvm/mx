6e24ea87de25c807f9698af1998709cf50081b59
Boris Spasojevic <boris.spasojevic@oracle.com>
2018-09-26 14:13:01 +0200

    Version bump.

--- mx.py
+++ mx.py
@@ -18671,7 +18671,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.185.4")  # Intellij ignore resource dependencies
+version = VersionSpec("5.186.0")  # GR-11731
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
