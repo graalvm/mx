2dc40e34ae1289c00a97b9b491b0a3dcac4347c5
Aleksandar Pejović <aleksandar.pejovic@oracle.com>
2018-10-01 15:29:09 +0200

    Bump version

--- mx.py
+++ mx.py
@@ -18669,7 +18669,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.185.0")  # Intellij Native Projects
+version = VersionSpec("5.185.1")  # GR-7929 checkstyle
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
