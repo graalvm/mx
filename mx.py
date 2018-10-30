4f80bfda00b265559eaa03b289f006093c602816
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2018-10-30 17:02:51 +0100

    Version update

--- mx.py
+++ mx.py
@@ -18705,7 +18705,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.190.8")  # GR-12187
+version = VersionSpec("5.190.10")  # GR-12187
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
