b2fcb0aa991a3de3810452180664fcf07e00ae55
Aleksandar Pejović <aleksandar.pejovic@oracle.com>
2018-11-26 13:00:26 +0100

    Bump version

--- mx.py
+++ mx.py
@@ -18835,7 +18835,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.194.3")  # GR-12454
+version = VersionSpec("5.194.4")  # GR-12711
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
