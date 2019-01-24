df1afa51a01b74c1122b1715702e32bc5b59671c
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2019-01-24 18:14:15 +0100

    Bump version

--- mx.py
+++ mx.py
@@ -19007,7 +19007,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.204.5")  # GR-13504
+version = VersionSpec("5.204.6")  # make_eclipse_attach
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
