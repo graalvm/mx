4e83b6a807b09aef767e05b0befa1911ddbf179f
Aleksandar Pejović <aleksandar.pejovic@oracle.com>
2019-01-29 13:43:51 +0100

    Bump version

--- mx.py
+++ mx.py
@@ -19012,7 +19012,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.205.0")  # [GR-13531] Fix running maven commands on Windows.
+version = VersionSpec("5.206.0")  # GR-13527
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
