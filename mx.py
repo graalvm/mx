282fbcd3e62b59268e89aa2f06ef48ee4f836d24
Paul Wögerer <paul.woegerer@oracle.com>
2019-03-06 11:35:36 +0100

    Bump version

--- mx.py
+++ mx.py
@@ -19054,7 +19054,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.213.6")  # GR-14280
+version = VersionSpec("5.213.7")  # [GR-14053] Fixes for building vm suite on Windows.
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
