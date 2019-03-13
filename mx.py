7cb7a182e92103f3b7fce5f8323ad38f22602076
Paul Wögerer <paul.woegerer@oracle.com>
2019-03-13 14:18:01 +0100

    Demote to warning if adding non-repo content to distribution

--- mx.py
+++ mx.py
@@ -2565,11 +2565,10 @@ class LayoutDistribution(AbstractDistribution):
                 return os.path.relpath(_source_file, files_root)
             _arcname_f = _rel_arcname
             if not self.suite.vc.locate(self.suite.vc_dir, file_path, abortOnError=False):
-                # TODO should always abort, tolerate absolute paths for now
                 absolute_source = isabs(source_path)
                 if absolute_source:
                     _arcname_f = lambda a: a
-                abort_or_warn("Adding file which is not in the repository: '{}' in '{}'".format(file_path, destination), not absolute_source, context=self)
+                warn("Adding file which is not in the repository: '{}' in '{}'".format(file_path, destination), context=self)
             elif isabs(source_path):
                 abort("Source should not be absolute: '{}' in '{}'".format(source_path, destination), context=self)
             _install_source_files(((source_file, _arcname_f(source_file)) for source_file in glob.iglob(file_path)), include=source_path, excludes=source.get('exclude'))
