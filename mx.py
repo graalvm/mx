760926b595e39d522616b77342f6e4360ef75354
Tom Rodriguez <tom.rodriguez@oracle.com>
2019-04-03 12:05:29 -0700

    Print out failing test methods in the format used for single run tests

--- mx.py
+++ mx.py
@@ -19160,7 +19160,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.215.6")  # GR-14974
+version = VersionSpec("5.215.7")  # GR-15074
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
