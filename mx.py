0fa4a67ae5121eef2a38b2f415f875104bb1646e
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2019-05-31 09:51:30 +0200

    Bump version

--- mx.py
+++ mx.py
@@ -19274,7 +19274,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.218.5")  # GR-16156
+version = VersionSpec("5.219.0")  # LINKY_LAYOUT
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
