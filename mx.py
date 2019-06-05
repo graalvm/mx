6298b7bc363dd3ec8c2a35ca215d4bea66db11b4
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2019-06-05 17:11:07 +0200

    Bump version

--- mx.py
+++ mx.py
@@ -19356,7 +19356,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.219.3")  # .cmd subst
+version = VersionSpec("5.220.0")  # paths
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
