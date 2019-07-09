9dd08b7757f00bf64b6e688d0c830f324deab63b
Jirka Marsik <jiri.marsik@oracle.com>
2019-07-09 11:55:16 +0200

    Update mx version

--- mx.py
+++ mx.py
@@ -19646,7 +19646,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.226.4")  # GR-17070
+version = VersionSpec("5.226.5")  # GR-13299
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
