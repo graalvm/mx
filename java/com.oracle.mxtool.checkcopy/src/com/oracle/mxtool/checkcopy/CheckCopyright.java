c51367c209cb230e1086bcfaa9f3adeb11304abb
Doug Simon <doug.simon@oracle.com>
2019-09-10 21:13:48 +0200

    use current year when fixing copyright header date

--- java/com.oracle.mxtool.checkcopy/src/com/oracle/mxtool/checkcopy/CheckCopyright.java
+++ java/com.oracle.mxtool.checkcopy/src/com/oracle/mxtool/checkcopy/CheckCopyright.java
@@ -289,6 +289,16 @@ public class CheckCopyright {
             return true;
         }
 
+        static class LazyHeader {
+            static {
+                System.out.printf("<year in copyright> != <year last modified> -> <current year>: <file>%n");
+            }
+
+            static void emit() {
+
+            }
+        }
+
         /**
          * Check the year info against the copyright header. N.B. In the case of multiple matching
          * groups, only the last group is checked. I.e., only the last lines containing year info is
@@ -309,11 +319,11 @@ public class CheckCopyright {
             String yearInCopyrightString = matcher.group(groupCount);
             yearInCopyright = Integer.parseInt(yearInCopyrightString);
             yearInCopyrightIndex = matcher.start(groupCount);
-            if (yearInCopyright != info.lastYear) {
-                System.out.println(fileName + " copyright last modified year " + yearInCopyright + ", " + vc.name() + " last modified year " + info.lastYear);
+            if (yearInCopyright != info.lastYear && currentYear != yearInCopyright) {
                 if (FIX.getValue()) {
                     // Use currentYear as that is what it will be when it's checked in!
-                    System.out.println("updating last modified year of " + fileName + " to " + info.lastYear);
+                    LazyHeader.emit();
+                    System.out.printf("%d != %d -> %d [fixed]: %s%n", yearInCopyright, info.lastYear, currentYear, fileName);
                     /*
                      * If the previous copyright only specified a single (initial) year, we convert
                      * it to the pair form
@@ -323,12 +333,14 @@ public class CheckCopyright {
                         // single year form
                         newContent += yearInCopyrightString + ", ";
                     }
-                    newContent += info.lastYear + fileContent.substring(yearInCopyrightIndex + 4);
+                    newContent += currentYear + fileContent.substring(yearInCopyrightIndex + 4);
                     final FileOutputStream os = new FileOutputStream(fileName);
                     os.write(newContent.getBytes());
                     os.close();
                     return true;
                 } else {
+                    LazyHeader.emit();
+                    System.out.printf("%d != %d -> %d: %s%n", yearInCopyright, info.lastYear, currentYear, fileName);
                     return false;
                 }
             }
