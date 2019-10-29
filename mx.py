15eb659560694ce665409e7de87c8dac882e6af8
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2019-10-29 19:05:13 +0100

    Bump version

--- mx.py
+++ mx.py
@@ -19226,7 +19226,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.243.0")  # On Windows, quote command line arguments containing the '=' char
+version = VersionSpec("5.244.0")  # maven-deploy --tags
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
