58af836aea33e574c798be56531d6f937e192d1a
Francois Farquet <francois.farquet@oracle.com>
2019-11-29 09:22:38 +0100

    mx version bump

--- mx.py
+++ mx.py
@@ -19323,7 +19323,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.247.7")  # GR-19726
+version = VersionSpec("5.247.8")  # GR-19801
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
