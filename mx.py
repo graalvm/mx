ad2ddd99ae3aaa541387ca8b3401c26103d58827
Francois Farquet <francois.farquet@oracle.com>
2020-01-17 17:01:47 +0100

    sclone --revision was ignored

--- mx.py
+++ mx.py
@@ -19351,7 +19351,7 @@ def main():
 
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.248.11")  # GR-20589
+version = VersionSpec("5.248.12")  # GR-20688
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
