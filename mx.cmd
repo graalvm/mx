1d8c4db51b559e4ffc9f7893dc7f28d236b693d9
Aleksandar Pejović <aleksandar.pejovic@oracle.com>
2020-01-30 15:46:32 +0100

    Make mx respect MX_PYTHON env var

--- mx.cmd
+++ mx.cmd
@@ -1,1 +1,9 @@
-@python -u "%~dp0mx.py" %*
+@echo off
+setlocal enableextensions
+
+:: prefer the interpreter specified by MX_PYTHON
+if defined MX_PYTHON (
+  %MX_PYTHON% -u "%~dp0mx.py" %*
+) else (
+  python -u "%~dp0mx.py" %*
+)
