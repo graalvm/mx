a11b0269708d9da993226e75a6503fe0c755d2a3
sessaadani <soufiane.essaadani@oracle.com>
2020-03-17 13:54:42 +0100

    Make mx.cmd respect  MX_PYTHON_VERSION

--- mx.cmd
+++ mx.cmd
@@ -4,6 +4,8 @@ setlocal enableextensions
 :: prefer the interpreter specified by MX_PYTHON
 if defined MX_PYTHON (
   %MX_PYTHON% -u "%~dp0mx.py" %*
+) else if defined MX_PYTHON_VERSION (
+  python%MX_PYTHON_VERSION% -u "%~dp0mx.py" %*
 ) else (
   python -u "%~dp0mx.py" %*
 )
