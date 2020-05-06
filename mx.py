d9224e0f4891e3312f5794eb7adfd5c1c043fe05
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2020-05-06 17:38:01 +0200

    Fix up version

--- mx.py
+++ mx.py
@@ -19516,7 +19516,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.262.3") # GR-16855-mx
+version = VersionSpec("5.263.0") # fetch-jdk
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
