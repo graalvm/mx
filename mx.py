1cd0382aac791604888b03b7d40cd9fefc462d96
stepan <stepan.sindelar@oracle.com>
2020-12-10 18:22:03 +0100

    Update the version

--- mx.py
+++ mx.py
@@ -17184,7 +17184,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.279.4")  # GR-27953
+version = VersionSpec("5.279.5")  # GR-28124
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
