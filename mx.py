0d1710131a7f5a4feed29c28a240f9e0e8835808
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2020-12-15 18:15:57 +0100

    The `mx checkout-downstream` command does not require suite loading.
    (cherry picked from commit ffea473cc8248a86274179cb467cb98df6eaf19f)

--- mx.py
+++ mx.py
@@ -17169,7 +17169,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.275.6")  # GR-27318
+version = VersionSpec("5.280.3")  # GR-28189
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
