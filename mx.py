b891741e59dd2f51692f6300cb24c03f30c673a5
Peter Hofer <peter.hofer@oracle.com>
2021-01-12 14:05:09 +0100

    Update version.

--- mx.py
+++ mx.py
@@ -17184,7 +17184,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.280.10")  # [GR-28545] Copyright check should not fail with a new year
+version = VersionSpec("5.280.11")  # [GR-28545] Copyright check should not fail with a new year
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
