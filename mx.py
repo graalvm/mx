68d7ec5ff94e868adc9cb59a0044b49ac830ce5b
Josef Eisl <josef.eisl@oracle.com>
2021-03-02 11:19:30 +0100

    Bump version

--- mx.py
+++ mx.py
@@ -17335,7 +17335,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.288.0")  # print @Ignore reason
+version = VersionSpec("5.288.1")  # custom distribution class
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
