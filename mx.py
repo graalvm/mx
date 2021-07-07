d0d27b55472196c6518c6304a3f0a209c7a09b69
Paul Wögerer <paul.woegerer@oracle.com>
2021-07-07 17:47:08 +0200

    Version bump

--- mx.py
+++ mx.py
@@ -17676,7 +17676,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.304.5")  # GR-32364
+version = VersionSpec("5.305.0")  # GR-32211
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
