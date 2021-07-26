8b74fc6f92f2654fb80b429f7be0d69419d35cb7
Doug Simon <doug.simon@oracle.com>
2021-07-26 13:04:31 +0200

    symlink should not be created before target exists

--- mx_jardistribution.py
+++ mx_jardistribution.py
@@ -321,14 +321,14 @@ class JARDistribution(mx.Distribution, mx.ClasspathDependency):
         bin_archive = _Archive(self, self.original_path(), exploded)
         src_archive = _Archive(self, self.sourcesPath, exploded) if not unified else bin_archive
 
-        # GR-31142
-        latest_bin_archive = join(self.suite.get_output_root(False, False), "dists", os.path.basename(bin_archive.path))
-        _stage_file_impl(bin_archive.path, latest_bin_archive)
-
         bin_archive.clean()
         src_archive.clean()
 
         stager = _ArchiveStager(bin_archive, src_archive, exploded)
+
+        # GR-31142
+        latest_bin_archive = join(self.suite.get_output_root(False, False), "dists", os.path.basename(bin_archive.path))
+        _stage_file_impl(bin_archive.path, latest_bin_archive)
 
         self.notify_updated()
         compliance = self._compliance_for_build()
