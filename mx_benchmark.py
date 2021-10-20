f17555328cbe43d77878a51acb30a9fc0b98577e
Francois Farquet <francois.farquet@oracle.com>
2021-10-20 18:19:16 +0200

    Simplify log message

--- mx_benchmark.py
+++ mx_benchmark.py
@@ -2427,8 +2427,7 @@ class BenchmarkExecutor(object):
             requested_benchmarks = [bench.strip() for bench in benchspec[2:-1].split(",")]
             if not set(requested_benchmarks) <= set(all_benchmarks):
                 difference = list(set(requested_benchmarks) - set(all_benchmarks))
-                plural = "" if len(difference) == 1 else "s"
-                mx.abort("The benchmark{0} {1} are not supported by the suite ".format(plural, ",".join(difference)))
+                mx.abort("Benchmarks not supported by the suite: ".format(",".join(difference)))
             return (suite, [[b] for b in all_benchmarks if b in requested_benchmarks])
         elif benchspec.startswith("r[") and benchspec.endswith("]"):
             all_benchmarks = suite.completeBenchmarkList(bmSuiteArgs)
@@ -2448,8 +2447,7 @@ class BenchmarkExecutor(object):
             excluded_benchmarks = [bench.strip() for bench in benchspec[1:-1].split(",")]
             if not set(excluded_benchmarks) <= set(all_benchmarks):
                 difference = list(set(excluded_benchmarks) - set(all_benchmarks))
-                plural = "" if len(difference) == 1 else "s"
-                mx.abort("The benchmark{0} {1} are not in the suite ".format(plural, ",".join(difference)))
+                mx.abort("Benchmarks not supported by the suite: ".format(",".join(difference)))
             return (suite, [[b] for b in all_benchmarks if b not in excluded_benchmarks])
         elif benchspec == "":
             return (suite, [None])
