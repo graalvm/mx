35a3789ca2087a72233d991ba24f233321e4a6d6
Soufiane Nassih <soufiane.nassih@oracle.com>
2021-11-04 16:22:49 +0100

    Update mx version

--- mx.py
+++ mx.py
@@ -17793,7 +17793,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.316.1")  # GR-34879
+version = VersionSpec("5.316.2")  # GR-34126
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
