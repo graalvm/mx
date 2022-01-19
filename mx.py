29426110d95cda4eefadb3fe2a67984535cab164
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2022-01-19 10:25:27 +0100

    Update version.

--- mx.py
+++ mx.py
@@ -17865,7 +17865,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.317.10")  # GR-33851 (javadocs)
+version = VersionSpec("5.317.11")  # GR-36461
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
