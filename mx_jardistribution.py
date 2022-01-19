5fe90519310161d20e63c32eacd1c1b6b3cc5a8c
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2022-01-19 11:57:12 +0100

    Make bail-out conditions for file staging more general.

--- mx_jardistribution.py
+++ mx_jardistribution.py
@@ -1417,11 +1417,14 @@ def _stage_file_impl(src, dst):
     """
     Copies or symlinks `src` to `dst`.
     """
-    if os.path.isdir(src) and os.path.isdir(dst) and os.path.normpath(src) == os.path.normpath(dst):
-        # GR-36461: If the directories are the same, then nothing should be done.
+    # GR-36461: If the directories are the same, then nothing should be done.
+    if not exists(src):
+        return
+    if exists(dst) and os.path.samefile(src, dst):
         return
 
     mx.ensure_dir_exists(dirname(dst))
+    
     if not mx.can_symlink():
         if exists(dst):
             os.remove(dst)
