4f8a95b133f49480d141277abf0a82b01f224333
Francois Farquet <francois.farquet@oracle.com>
2022-04-04 16:42:45 +0200

    mx version bump

--- mx.py
+++ mx.py
@@ -17903,7 +17903,7 @@ def main():
 
 
 # The version must be updated for every PR (checked in CI)
-version = VersionSpec("5.320.5")  # GR-37557
+version = VersionSpec("5.320.6")  # GR-37557 fix
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
