7b4f49d714d5e8ffe37fa6b2a004bda3eea92bdf
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2023-02-06 13:12:56 +0100

    Bump version

--- mx.py
+++ mx.py
@@ -18371,7 +18371,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("6.14.21") # GR-42205
+version = VersionSpec("6.15.0") # musl
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
