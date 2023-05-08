9a2d251e378933b91032a18e8afb13a3e7b4f76b
Andreas Woess <andreas.woess@oracle.com>
2023-05-08 18:15:43 +0200

    Fix passing arguments that contain parentheses on windows.

--- mx.cmd
+++ mx.cmd
@@ -2,10 +2,6 @@
 setlocal enableextensions
 
 :: prefer the interpreter specified by MX_PYTHON
-if defined MX_PYTHON (
-  %MX_PYTHON% -u "%~dp0mx.py" %*
-) else if defined MX_PYTHON_VERSION (
-  python%MX_PYTHON_VERSION% -u "%~dp0mx.py" %*
-) else (
-  python -u "%~dp0mx_enter.py" %*
-)
+if defined MX_PYTHON %MX_PYTHON% -u "%~dp0mx.py" %* & goto :eof
+if defined MX_PYTHON_VERSION python%MX_PYTHON_VERSION% -u "%~dp0mx.py" %* & goto :eof
+python -u "%~dp0mx_enter.py" %*
