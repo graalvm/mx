a0eccfddcd0ab82bbba6b68e3dda5872f907a415
Andreas Woess <andreas.woess@oracle.com>
2023-05-08 18:34:15 +0200

    Add note about use of parentheses in batch scripts.

--- mx.cmd
+++ mx.cmd
@@ -1,6 +1,8 @@
 @echo off
 setlocal enableextensions
 
+:: note: do not use parentheses around commands because that will break passing arguments that contain parentheses (see https://ss64.com/nt/syntax-brackets.html)
+
 :: prefer the interpreter specified by MX_PYTHON
 if defined MX_PYTHON %MX_PYTHON% -u "%~dp0mx.py" %* & goto :eof
 if defined MX_PYTHON_VERSION python%MX_PYTHON_VERSION% -u "%~dp0mx.py" %* & goto :eof
