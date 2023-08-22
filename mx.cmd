da3e8af1c2b5c1b53b4a189caf6082cfa8ec0156
Patrick Ziegler <patrick.ziegler@oracle.com>
2023-08-22 16:14:49 +0000

    Run mx module in mx.cmd

--- mx.cmd
+++ mx.cmd
@@ -13,4 +13,4 @@ if defined MX_PYTHON (
 set "PYTHONPATH=%~dp0/src;%~dp0/oldnames"
 
 :: local variables can be used after endlocal if on the same line
-endlocal & "%python_exe%" -u "%~dp0mx_enter.py" %*
+endlocal & "%python_exe%" -u -m mx %*
