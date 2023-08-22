2a79ca33a38db09815651fa721d4f346074352e7
Rudi Horn <rudi.horn@oracle.com>
2023-08-22 14:14:02 +0000

    Fix Windows mx.cmd

--- mx.cmd
+++ mx.cmd
@@ -10,5 +10,7 @@ if defined MX_PYTHON (
   set "python_exe=python"
 )
 
+set "PYTHONPATH=%~dp0/src;%~dp0/oldnames"
+
 :: local variables can be used after endlocal if on the same line
 endlocal & "%python_exe%" -u "%~dp0mx_enter.py" %*
