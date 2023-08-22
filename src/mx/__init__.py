6f3f6d7c992d4b8f62166c45b57d83d6363d0d1e
Patrick <patrick.ziegler@oracle.com>
2023-08-22 16:17:12 +0200

    Add mx._replaceResultsVar to oldnames exports

--- src/mx/__init__.py
+++ src/mx/__init__.py
@@ -1,4 +1,4 @@
 from ._impl.mx import *
 
-# For some reason these private symbols is used externally
-from ._impl.mx import _mx_path, _opts
+# For some reason these private symbols are used externally
+from ._impl.mx import _mx_path, _opts, _replaceResultsVar
