9a816d4aab98878c23df9b5215655905fe7d9dc7
Josef Eisl <josef.eisl@oracle.com>
2023-09-15 10:02:52 +0200

    Bump version

--- mx.py
+++ mx.py
@@ -18789,7 +18789,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("6.49.0")  # code owners
+version = VersionSpec("6.49.1")  # fetch-jdk labsjdk-ee-latest
 
 _mx_start_datetime = datetime.utcnow()
 _last_timestamp = _mx_start_datetime
