78780aa68e294fbdea72452ad32149459a102f84
Doug Simon <doug.simon@oracle.com>
2023-09-16 13:20:07 +0200

    version bump and sync of common files

--- mx.py
+++ mx.py
@@ -18789,7 +18789,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("6.50.0")  # GR-48667 Scan SuiteClasses for @AddExports
+version = VersionSpec("6.50.1")  # GR-47930 - Improve SDK lookup in intellijinit.
 
 _mx_start_datetime = datetime.utcnow()
 _last_timestamp = _mx_start_datetime
