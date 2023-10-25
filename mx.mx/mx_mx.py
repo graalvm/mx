01ed6c70aaeba56ed7547d80c04e04eb9e34d260
Patrick <patrick.ziegler@oracle.com>
2023-10-25 18:38:26 +0200

    Add proof of concept mx pyformat
    Only meant for formatting the mx source code

---
+++ mx.mx/mx_mx.py
@@ -0,0 +1,65 @@
+# ----------------------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# This code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 only, as
+# published by the Free Software Foundation.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details (a copy is included in the LICENSE file that
+# accompanied this code).
+#
+# You should have received a copy of the GNU General Public License version
+# 2 along with this work; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+# or visit www.oracle.com if you need additional information or have any
+# questions.
+#
+# ----------------------------------------------------------------------------------------------------
+import argparse
+
+import mx
+
+suite = mx.suite("mx")
+
+
+@mx.command(suite.name, "pyformat")
+@mx.no_suite_loading
+def pyformat(arg_list: [str]):
+    parser = argparse.ArgumentParser(prog="mx pyformat", description="TODO document")
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        action="store_true",
+        help="Do not write files to disk. Will abort if any files have formatting errors",
+    )
+    parser.add_argument(
+        "source_files",
+        metavar="FILE",
+        nargs="*",
+        help="Source files to format (formats all by default)",
+    )
+    args = parser.parse_args(arg_list)
+
+    source_files: [str] = args.source_files
+
+    if not source_files:
+        # By default, we just pass the location of the mx suite and let black do the file discovery
+        # Exclude rules can be added to pyproject.toml
+        source_files = [suite.dir]
+
+    black_args: [str] = []
+
+    if args.dry_run:
+        black_args += ["--check", "--diff", "--color"]
+
+    if mx._opts.verbose:
+        black_args += ["--verbose"]
+
+    mx.run(["black"] + black_args + ["--"] + source_files)
