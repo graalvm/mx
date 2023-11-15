9aaa0a62f7d200bcad85ef7652f1499d3faa0a58
stepan <stepan.sindelar@oracle.com>
2023-11-15 18:09:00 +0100

    IntelliJ: refactor the code to use IntellijConfig for all the options and flags

--- src/mx/_impl/mx_ide_intellij.py
+++ src/mx/_impl/mx_ide_intellij.py
@@ -56,40 +56,39 @@ intellij_ruby_sdk_type = 'RUBY_SDK'
 
 @dataclass
 class IntellijConfig:
-    import_inner_classes: bool = False
+    python_projects: bool = True
+    external_projects: bool = True
+    java_modules: bool = True
+    native_projects: bool = False
     max_java_compliance: int = 16
+    import_inner_classes: bool = False
     on_save_actions: bool = False
+    refresh_only: bool = False,
+    do_fsck_projects: bool = True,
+    args: ... = None
 
 
 @mx.command('mx', 'intellijinit')
 def intellijinit_cli(args):
     """(re)generate Intellij project configurations"""
     parser = ArgumentParser(prog='mx ideinit')
-    parser.add_argument('--no-python-projects', action='store_false', dest='pythonProjects', help='Do not generate projects for the mx python projects.')
-    parser.add_argument('--no-external-projects', action='store_false', dest='externalProjects', help='Do not generate external projects.')
-    parser.add_argument('--no-java-projects', '--mx-python-modules-only', action='store_false', dest='javaModules', help='Do not generate projects for the java projects.')
-    parser.add_argument('--native-projects', action='store_true', dest='nativeProjects', help='Generate native projects.')
+    parser.add_argument('--no-python-projects', action='store_false', dest='python_projects', help='Do not generate projects for the mx python projects.')
+    parser.add_argument('--no-external-projects', action='store_false', dest='external_projects', help='Do not generate external projects.')
+    parser.add_argument('--no-java-projects', '--mx-python-modules-only', action='store_false', dest='java_modules', help='Do not generate projects for the java projects.')
+    parser.add_argument('--native-projects', action='store_true', dest='native_projects', help='Generate native projects.')
     parser.add_argument('--max-java-compliance', dest='max_java_compliance', type=int, default=16, help='Cap the Java compliance at this value. IntelliJ requires an acceptance of a legal notice for beta Java specifications.')
     parser.add_argument('--import-inner-classes', action='store_true', dest='import_inner_classes', help='Configure auto-import to insert inner class imports.')
     parser.add_argument('--on-save-actions', action='store_true', dest='on_save_actions', help='Generate On Save Actions: checkstyle format and optimize imports.')
-    parser.add_argument('remainder', nargs=REMAINDER, metavar='...')
+    parser.add_argument('args', nargs=REMAINDER, metavar='...')
 
     extra_args = os.environ.get('MX_INTELLIJINIT_DEFAULTS', '').strip().split(' ')
     if extra_args:
         mx.log("Applying extra arguments from MX_INTELLIJINIT_DEFAULTS environment variable")
-    args = parser.parse_args(extra_args + args)
-
-    config = IntellijConfig()
-    for f in fields(IntellijConfig):
-        setattr(config, f.name, getattr(args, f.name))
-
-    intellijinit(args.remainder, mx_python_modules=args.pythonProjects, java_modules=args.javaModules,
-                 generate_external_projects=args.externalProjects, native_projects=args.nativeProjects,
-                 config=config)
+    config = parser.parse_args(extra_args + args, namespace=IntellijConfig())
+    intellijinit(config)
 
 
-def intellijinit(args, refreshOnly=False, doFsckProjects=True, mx_python_modules=True, java_modules=True,
-                 generate_external_projects=True, native_projects=False, config=IntellijConfig()):
+def intellijinit(config: IntellijConfig):
     # In a multiple suite context, the .idea directory in each suite
     # has to be complete and contain information that is repeated
     # in dependent suites.
@@ -99,18 +98,17 @@ def intellijinit(args, refreshOnly=False, doFsckProjects=True, mx_python_modules
 
     suites = mx.suites(True)
 
-    if mx_python_modules and mx._mx_suite not in suites:
+    if config.python_projects and mx._mx_suite not in suites:
         suites.append(mx._mx_suite)
 
     for suite in suites:
-        _intellij_suite(args, suite, declared_modules, referenced_modules, sdks, refreshOnly, mx_python_modules,
-                        generate_external_projects, java_modules and not suite.isBinarySuite(), suite != mx.primary_suite(),
-                        generate_native_projects=native_projects, config=config)
+        config.java_modules = config.java_modules and not suite.isBinarySuite()
+        _intellij_suite(suite, declared_modules, referenced_modules, sdks, suite != mx.primary_suite(), config)
 
     if len(referenced_modules - declared_modules) != 0:
         mx.abort(f'Some referenced modules are missing from modules.xml: {referenced_modules - declared_modules}')
 
-    if mx_python_modules:
+    if config.python_projects:
         # Module for the MX source code
         module_src = mx.XMLDoc()
         module_src.open('module', attributes={'type': 'PYTHON_MODULE', 'version': '4'})
@@ -141,7 +139,7 @@ def intellijinit(args, refreshOnly=False, doFsckProjects=True, mx_python_modules
         module_tests.close('module')
         mx.update_file(join(mx._mx_suite.dir, 'tests', 'mx_tests.iml'), module_tests.xml(indent='  ', newl='\n'))
 
-    if doFsckProjects and not refreshOnly:
+    if config.do_fsck_projects and not config.refresh_only:
         mx_ideconfig.fsckprojects([])
 
 def intellij_read_sdks():
@@ -286,9 +284,7 @@ def _intellij_library_file_name(library_name, unique_library_file_names):
     return file_name
 
 
-def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refreshOnly=False, mx_python_modules=False,
-                    generate_external_projects=True, java_modules=True, module_files_only=False, generate_native_projects=False,
-                    config=IntellijConfig()):
+def _intellij_suite(s, declared_modules, referenced_modules, sdks, module_files_only: bool, config: IntellijConfig):
     libraries = set()
     jdk_libraries = set()
 
@@ -451,14 +447,14 @@ def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refresh
         create_intellij_module(resource_search_path, "ci")
         create_intellij_module(resource_search_path, "docs")
 
-    if generate_external_projects:
+    if config.external_projects:
         for p in s.projects_recursive() + mx._mx_suite.projects_recursive():
             _intellij_external_project(getattr(p, 'externalProjects', None), sdks, p)
 
     max_checkstyle_version = None
     compilerXml = None
 
-    if java_modules:
+    if config.java_modules:
         if not module_files_only:
             compilerXml = mx.XMLDoc()
             compilerXml.open('project', attributes={'version': '4'})
@@ -619,7 +615,7 @@ def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refresh
             compilerXml.close('option')
             compilerXml.close('component')
 
-    if mx_python_modules:
+    if config.python_projects:
 
         def _python_module(suite):
             """
@@ -682,10 +678,10 @@ def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refresh
             moduleFilePath = "$PROJECT_DIR$/" + os.path.relpath(mx_tests_module_file, project_dir)
             modulesXml.element('module', attributes={'fileurl': 'file://' + moduleFilePath, 'filepath': moduleFilePath})
 
-    if generate_native_projects:
+    if config.native_projects:
         _intellij_native_projects(s, module_files_only, declared_modules, modulesXml)
 
-    if generate_external_projects:
+    if config.external_projects:
         _intellij_external_project(s.suiteDict.get('externalProjects', None), sdks, s)
 
     if not module_files_only:
@@ -695,7 +691,7 @@ def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refresh
         moduleXmlFile = join(ideaProjectDirectory, 'modules.xml')
         mx.update_file(moduleXmlFile, modulesXml.xml(indent='  ', newl='\n'))
 
-    if java_modules and not module_files_only:
+    if config.java_modules and not module_files_only:
         unique_library_file_names = set()
         librariesDirectory = mx.ensure_dir_exists(join(ideaProjectDirectory, 'libraries'))
 
@@ -812,7 +808,7 @@ def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refresh
         miscXml = mx.XMLDoc()
         miscXml.open('project', attributes={'version' : '4'})
 
-        if java_modules:
+        if config.java_modules:
             mainJdk = mx.get_jdk()
             miscXml.open('component', attributes={'name' : 'ProjectRootManager', 'version': '2', 'languageLevel': _complianceToIntellijLanguageLevel(mainJdk.javaCompliance), 'project-jdk-name': intellij_get_java_sdk_name(sdks, mainJdk), 'project-jdk-type': intellij_java_sdk_type})
             miscXml.element('output', attributes={'url' : 'file://$PROJECT_DIR$/' + os.path.relpath(s.get_output_root(), s.dir)})
@@ -844,7 +840,7 @@ def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refresh
         mx.ensure_dir_exists(join(ideaProjectDirectory, 'runConfigurations'))
         mx.update_file(runConfigFile, runConfig.xml(indent='  ', newl='\n'))
 
-        if java_modules:
+        if config.java_modules:
             # Eclipse formatter config
             corePrefsSources = s.eclipse_settings_sources().get('org.eclipse.jdt.core.prefs')
             uiPrefsSources = s.eclipse_settings_sources().get('org.eclipse.jdt.ui.prefs')
@@ -900,7 +896,7 @@ def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refresh
                 miscFile = join(ideaProjectDirectory, 'eclipseCodeFormatter.xml')
                 mx.update_file(miscFile, miscXml.xml(indent='  ', newl='\n'))
 
-        if java_modules:
+        if config.java_modules:
             # Write codestyle settings
             mx.ensure_dir_exists(join(ideaProjectDirectory, 'codeStyles'))
 
@@ -1070,7 +1066,7 @@ def _intellij_suite(args, s, declared_modules, referenced_modules, sdks, refresh
         vcsXml.open('project', attributes={'version': '4'})
         vcsXml.open('component', attributes={'name': 'VcsDirectoryMappings'})
 
-        suites_for_vcs = mx.suites() + ([mx._mx_suite] if mx_python_modules else [])
+        suites_for_vcs = mx.suites() + ([mx._mx_suite] if config.python_projects else [])
         sourceSuitesWithVCS = [vc_suite for vc_suite in suites_for_vcs if vc_suite.isSourceSuite() and vc_suite.vc is not None]
         uniqueSuitesVCS = {(vc_suite.vc_dir, vc_suite.vc.kind) for vc_suite in sourceSuitesWithVCS}
         for vcs_dir, kind in uniqueSuitesVCS:
