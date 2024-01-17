a3abefcbc89706e1613a651d423cb6c4ba799c24
Patrick <patrick.ziegler@oracle.com>
2024-01-17 14:52:47 +0100

    Bump mx version

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -19255,7 +19255,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.7.1")  # [GR-51151] fix long pathname on windows
+version = VersionSpec("7.7.2")  # [GR-51431] Remove repairing of benchmark datapoints
 
 _mx_start_datetime = datetime.utcnow()
 _last_timestamp = _mx_start_datetime
