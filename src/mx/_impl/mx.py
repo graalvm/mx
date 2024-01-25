aedc9d1165022c6bf7e98662671f802419e62f75
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2024-01-25 18:17:39 +0100

    Bump version

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -19243,7 +19243,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.8.1")  # GR-49806 gate mx on JDK latest
+version = VersionSpec("7.8.2")  # cygwin git
 
 _mx_start_datetime = datetime.utcnow()
 
