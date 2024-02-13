f869c82532ebada66c6f94b44a868c7e7f538a7e
Andreas Woess <andreas.woess@oracle.com>
2024-02-13 20:45:17 +0100

    Revert "Add mx command to print the module path."

--- src/mx/_impl/mx_javamodules.py
+++ src/mx/_impl/mx_javamodules.py
@@ -1480,8 +1480,3 @@ def module_path_entries(names=None, jdk=None, includeSelf=True, includeProjects=
                 indirect="transitively" if indirect else "directly",
             ))
     return (dep_with_requires(*e) for e in mpEntries.items())
-
-@mx.command('mx', 'modulepath')
-def print_modulepath(args):
-    """Print module path for the specified dependencies"""
-    print(":".join(e.classpath_repr() for e, _, _ in module_path_entries(args, mx.get_jdk())))
