62d27323d7c31268f450c72c94700dbd4d219dfc
Andreas Woess <andreas.woess@oracle.com>
2024-02-28 15:30:37 +0100

    Bump mx version.

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -19293,7 +19293,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.14.1")  # mergetool-suite-import should reset to LOCAL import revision
+version = VersionSpec("7.14.2")  # revert GR-51052
 
 _mx_start_datetime = datetime.utcnow()
 
