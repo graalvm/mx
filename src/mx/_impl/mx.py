5a8d16d22260ea71ce52476330ef19bf5f98dbf4
Josef Eisl <josef.eisl@oracle.com>
2024-04-11 11:48:37 +0200

    Bump version

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18149,7 +18149,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.22.3")  # [GR-53394] Fix spurious validation error for MavenProject.
+version = VersionSpec("7.22.4")  # Support short JAVA_HOME names on darwin
 
 _mx_start_datetime = datetime.utcnow()
 
