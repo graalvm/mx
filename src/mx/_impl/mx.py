8019a4db5d31e2af89fef97a9170e3add6653c60
Andrija Kolic <andrija.kolic@oracle.com>
2024-04-17 17:48:54 +0200

    Stop the MaxRssCopyRule of the rsspercentiles tracker from rerunning and producing incorrect, unwanted max-rss values for image build.

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18149,7 +18149,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.22.5")  # Support .cpp and .hpp in DefaultNinjaProject
+version = VersionSpec("7.22.6")  # Stop MaxRssCopyRule of the rsspercentiles tracker from rerunning
 
 _mx_start_datetime = datetime.utcnow()
 
