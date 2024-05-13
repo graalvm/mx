d4efd4bfc30ad36701a33714efe225de99c709e8
Patrick Ziegler <patrick.ziegler@oracle.com>
2024-05-13 16:51:56 +0200

    Bump mx version

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18173,7 +18173,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.25.2")  # GR-53991
+version = VersionSpec("7.25.3")  # GR-704 Remove --vm and --vmbuild flags
 
 _mx_start_datetime = datetime.utcnow()
 
