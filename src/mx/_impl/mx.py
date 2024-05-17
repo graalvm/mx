5dd00e53561f1b37f6ec965a6bdacf00461fa220
Andrija Kolic <andrija.kolic@oracle.com>
2024-05-17 10:31:14 +0200

    Add implicit owners inheritance to codeowners, add 'overwrite_parent' property that disables inheritance.

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18173,7 +18173,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.25.7")  # GR-54207 - improve diagnostics for unknown CompilerDaemon requests
+version = VersionSpec("7.25.8")  # GR-48017 Add implicit rule inheritance to codeowners
 
 _mx_start_datetime = datetime.utcnow()
 
