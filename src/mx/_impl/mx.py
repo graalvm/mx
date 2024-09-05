3cfe9c5a3ff041999643c9bb5ccb58df645345eb
akolic <andrija.kolic@oracle.com>
2024-09-05 10:44:30 +0200

    Replace misstyped 'raise' with 'return'.

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18196,7 +18196,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.29.6")  # GR-56803
+version = VersionSpec("7.29.7")  # GR-57947 replace misstyped 'raise' with 'return'
 
 _mx_start_datetime = datetime.utcnow()
 
