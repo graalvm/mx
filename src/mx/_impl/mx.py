ea3421ad06cd03440e30cf19d453212636d98649
Fengyun Liu <fengyun.liu@oracle.com>
2024-10-01 15:38:40 +0200

    Update version

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18204,7 +18204,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.32.1")  # GR-58516] Added support for specifying a JDK selector on the fetch-jdk command line
+version = VersionSpec("7.32.2")  # [GR-50987] Use correct bench-suite name if missing
 
 _mx_start_datetime = datetime.utcnow()
 
