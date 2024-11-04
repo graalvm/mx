4b73258352f2691101473a908346e494bb2801fa
Francois Farquet <francois.farquet@oracle.com>
2024-11-04 11:49:12 +0100

    mx version bump

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18215,7 +18215,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.33.2")  # [GR-59265] Mx Unittest Can't Find test Classes With @Theory Annotation Only
+version = VersionSpec("7.34.0")  # [GR-58270] Allow syntactic sugar to configure benchmark VM configs
 
 _mx_start_datetime = datetime.utcnow()
 
