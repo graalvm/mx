1d2c888a4a69481d01bdfc4c649d169e3f4fc3f5
Aleksandar Pejović <aleksandar.pejovic@oracle.com>
2024-11-29 12:34:49 +0100

    Bump version

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18224,7 +18224,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.35.2")  # [GR-59878] Exit CompilerDaemon when a SocketException is received
+version = VersionSpec("7.35.3")  # GR-11350
 
 _mx_start_datetime = datetime.utcnow()
 
