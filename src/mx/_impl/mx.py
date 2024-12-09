6779a3e22d60173ad51b45abc5f5400a260fbb06
Roland Schatz <roland.schatz@oracle.com>
2024-12-09 12:28:03 +0100

    Bump version.

--- src/mx/_impl/mx.py
+++ src/mx/_impl/mx.py
@@ -18365,7 +18365,7 @@ def main():
 _CACHE_DIR = get_env('MX_CACHE_DIR', join(dot_mx_dir(), 'cache'))
 
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = VersionSpec("7.36.1")  # [GR-53812] Fix crash in `mx benchmark`
+version = VersionSpec("7.36.2")  # [GR-60385] Catch exceptions communicating with compiler daemon.
 
 _mx_start_datetime = datetime.utcnow()
 
