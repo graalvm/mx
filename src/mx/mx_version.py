e4a1bb27076a4b53e31a7782e47302eb11a7866f
Ayman Moustadrif <ayman.moustadrif@oracle.com>
2025-06-23 17:59:33 +0100

    Bump mx version

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.56.0"  # GR-63250: Fetch-jdk: Add architecture to JDK path
+version = "7.57.0"  # GR-59039 Fix energy regex pattern
