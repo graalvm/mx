6f7e04bf91818ddda04a60f2a37cb6fc62b22d36
Roland Schatz <roland.schatz@oracle.com>
2025-06-25 12:45:55 +0200

    Bump version.

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.57.1"  # GR-66368 update to jacoco 0.8.13
+version = "7.57.2"  # GR-66473 fix unnecessary cmake run
