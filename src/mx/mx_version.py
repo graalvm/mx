44eee127a77ebc98e86f049c8e49148255ff5be8
Andrija Kolic <andrija.kolic@oracle.com>
2025-06-30 14:21:44 +0200

    Add 'supported_trackers' property to CustomHarnessBenchmarkSuite class; export EnergyConsumptionTracker.

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.58.2"  # GR-66921
+version = "7.58.3"  # GR-65510: Add CustomHarnessBenchmarkSuite.supported_trackers property
