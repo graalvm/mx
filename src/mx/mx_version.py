a60f2ea566f5769f6dbdc4041df4e02e3101a44c
Josef Eisl <josef.eisl@oracle.com>
2025-07-04 08:54:10 +0200

    bump version

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.58.3"  # GR-65510: Add CustomHarnessBenchmarkSuite.supported_trackers property
+version = "7.58.4"  # GR-66372: gc-jdks should ignore symlinks
