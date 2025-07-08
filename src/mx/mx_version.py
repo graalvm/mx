115ae5fd5edfbee2bfcf4f5235c184f283936def
Roland Schatz <roland.schatz@oracle.com>
2025-07-08 15:09:54 +0200

    Bump version.

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.62.2"  # GR-67014 Add ECJ ver 3.43
+version = "7.63.0"  # GR-67309 opt out of common `mx gate` tasks
