e7b1328ec888d0b78fe89437a0139a01623ce4f4
Francois Farquet <francois.farquet@oracle.com>
2025-09-24 16:57:06 +0200

    Bump mx version

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.62.0"  # GR-69926 Add --prebuilt-vm support to mx benchmark
+version = "7.62.1"  # GR-70009 --prebuilt-vm must be a string
