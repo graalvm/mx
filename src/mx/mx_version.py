bcfce2968ef5d395acad39833b15514644524583
Andrija Kolic <andrija.kolic@oracle.com>
2025-11-10 19:23:31 +0100

    Remove classes and methods deprecated by the benchmark execution context rework.

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.67.1"  # GR-69970 Code owners: do not suggest author as reviewer
+version = "7.67.2"  # GR-70656 remove classes and methods deprecated by the benchmark execution context rework
