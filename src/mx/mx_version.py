156851bea09a3389e4da5bc720fe334c7c146b2c
Matt D'Souza <matt.dsouza@oracle.com>
2025-11-14 12:29:46 -0500

    Move skip_platform_unsupported_benchmark to base BenchmarkDispatcher class

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.68.1"  # GR-70656 - Revert premature removal of deprecated interface
+version = "7.68.2"  # GR-71112 Move skip_platform_unsupported_benchmark to base BenchmarkDispatcher class
