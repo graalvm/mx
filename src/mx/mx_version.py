4088abd43e95de8ee044105fb8e8207628127d26
Roland Schatz <roland.schatz@oracle.com>
2025-12-15 16:30:22 +0100

    Bump version.

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.68.8"  # GR-71808 C version of timing wrapper
+version = "7.68.9"  # GR-72042 missing directory symlinks in layout dir distributions
