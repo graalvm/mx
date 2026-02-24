3442526bfc2f487f0ebd98b9e52c48e22eba237a
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2026-02-24 12:11:09 +0100

    Do not embed Ninja for windows/aarch64.

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.82.0"  # [GR-59303] Support delegation of suite release metadata.
+version = "7.82.1"  # [GR-73616] Do not embed Ninja for windows/aarch64
