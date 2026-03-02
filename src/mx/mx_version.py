59fe883cc249e4d5d7c24232af5a664059cf8223
Francois Farquet <francois.farquet@oracle.com>
2026-03-02 17:15:50 +0100

    mx version bump

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.82.1"  # [GR-73616] Do not embed Ninja for windows/aarch64
+version = "7.82.2"  # [GR-72678] Crema VM extraction fallback
