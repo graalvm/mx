55109e1c752e812fa94cc24f595ef76affcd7fbe
Felix Berlakovich <felix.berlakovich@oracle.com>
2026-03-04 18:25:08 +0100

    Sync common CI and bump mx version for pylint 3.2 PR

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.72.1"  # [GR-73836] Fix concurrent issues
+version = "7.72.2"  # [GR-73871] Allow pylint 3.2 in mx pylint
