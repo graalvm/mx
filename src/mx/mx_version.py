31bb624ed998752013ae9c4ef44bfbe0cf089d8b
Doug Simon <doug.simon@oracle.com>
2026-03-11 09:34:01 +0100

    bump version

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.74.1"  # [GR-73957] Add support for `mx fetch-jdk default`
+version = "7.74.2"  # [GR-73966] Use "lookup:default" if neither --java-home nor JAVA_HOME are set
