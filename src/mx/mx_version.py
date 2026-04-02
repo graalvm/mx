559730b6fd608cfe84814b8e17a05d6354751cb4
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2026-04-02 22:32:09 +0200

    Bump mx version

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.79.0"  # Add workspace mode for ideinit in multi-repository IntelliJ setups
+version = "7.79.1"  # GR-74496 - Simplify formatting rules
