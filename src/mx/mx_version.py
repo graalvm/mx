8f931c756208231090f01aafd5f152c80c19434e
Andreas Woess <andreas.woess@oracle.com>
2026-04-07 00:36:00 +0200

    Bump mx version.

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.78.4"  # GR-74354 - Defer suite version conflict resolution during discovery
+version = "7.78.5"  # GR-74532 - Fix sforceimports regression from stale cached git HEAD
