08675cdc591f651b7d5c9ce04e40b5b9601ae54e
Andreas Woess <andreas.woess@oracle.com>
2026-04-07 18:02:09 +0200

    Bump mx version.

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.78.5"  # GR-74532 - Fix sforceimports regression from stale cached git HEAD
+version = "7.78.6"  # GR-74729 - Fix maven-deploy compatibility with older Maven versions
