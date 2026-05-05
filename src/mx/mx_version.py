a3988b02bacd868c83c21b17164142123c99a661
Josef Eisl <josef.eisl@oracle.com>
2026-05-05 15:59:56 +0200

    Bump mx version

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.79.3"  # GR-75179 - Model optional dependencies in mx.
+version = "7.80.0"  # GR-75354 add gc-cache command
