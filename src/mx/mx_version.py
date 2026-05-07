4ec06b01e804bdc9287184a2a4b7f2d08bd5abcf
Josef Eisl <josef.eisl@oracle.com>
2026-05-07 10:49:26 +0200

    [GR-75420] Bump mx version

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.80.0"  # GR-75354 add gc-cache command
+version = "7.80.1"  # GR-75420 fix gc-cache older-than years parsing
