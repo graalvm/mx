0c48b1009c5e6d8afe7309728b008a3c1aaa4fe3
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2026-05-13 17:14:20 +0200

    Bump version

--- src/mx/mx_version.py
+++ src/mx/mx_version.py
@@ -1,2 +1,2 @@
 # The version must be updated for every PR (checked in CI) and the comment should reflect the PR's issue
-version = "7.82.2"  # [GR-72678] Crema VM extraction fallback
+version = "7.82.3"  # GR-75594 Fix ignore for `.swp` files in native projects
